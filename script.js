fbbd29909bec45f8b9e269f1c69bf1e36b1ed63a
Armand-Lluka <armandwork@hotmail.co.uk>
2019-01-05 19:03:39 +0000

    can filter through categories

--- script.js
+++ script.js
@@ -5,39 +5,44 @@
  * -------------------------------------------
  */
 
-const targetInput = document.getElementById('search'),
+// mockData = {
+//   chair: [
+//     { company: 'Renberget', type: 'swivel chair', price: '49.99' },
+//     { company: 'Stefan', type: 'chair', price: '17.99' },
+//     { company: 'Harry', type: 'chair', price: '34.99' },
+//     { company: 'Odvar', type: 'stool', price: '9.99' },
+//     { company: 'Beachmon Wing', type: 'chair', price: '249' },
+//     { company: 'Ekenaset', type: 'armchair', price: '179' },
+//     { company: 'Hemstanas', type: 'high-back armchair', price: '199' },
+//     { company: 'Ingolf', type: 'chair', price: '54.99' },
+//     { company: 'Fanbyn', type: 'chair', price: '59.99' },
+//     { company: 'Tobias', type: 'chair', price: '59.99' }
+//   ]
+//  }
+let targetInput = document.getElementById('search'),
   results = document.getElementById('list-container'),
-  mockData = {
-    chair: [
-      { company: 'Renberget', type: 'swivel chair', price: '49.99' },
-      { company: 'Stefan', type: 'chair', price: '17.99' },
-      { company: 'Harry', type: 'chair', price: '34.99' },
-      { company: 'Odvar', type: 'stool', price: '9.99' },
-      { company: 'Beachmon Wing', type: 'chair', price: '249' },
-      { company: 'Ekenaset', type: 'armchair', price: '179' },
-      { company: 'Hemstanas', type: 'high-back armchair', price: '199' },
-      { company: 'Ingolf', type: 'chair', price: '54.99' },
-      { company: 'Fanbyn', type: 'chair', price: '59.99' },
-      { company: 'Tobias', type: 'chair', price: '59.99' }
-    ]
-  },
+  mockData = ['Albania', 'Bahrain', 'Croatia', 'Denmark', 'Egypt'],
   matches = [];
 
 targetInput.focus();
 
-targetInput.addEventListener('keyup', e => {
+targetInput.addEventListener('keyup', function(event) {
   /*
    * Key Codes
-   * Enter : 13
+   * Enter: 13
    * Arrow up: 38
    * Arrow down: 40
    */
 
   results.innerHTML = '';
-  toggleResults('Hide');
+  toggleResults('hide');
 
   if (this.value.length > 0) {
     matches = getMatches(this.value);
+
+    if (matches.length > 0) {
+      displayMatches(matches);
+    }
   }
 });
 
@@ -48,4 +53,35 @@ const toggleResults = a => {
   } else if (a == 'hide') {
     results.classList.remove('visible');
   }
+};
+
+const getMatches = input => {
+  let matchList = [];
+
+  for (var i = 0; i < mockData.length; i++) {
+    if (
+      mockData[i].toLocaleLowerCase().indexOf(input.toLocaleLowerCase()) != -1
+    ) {
+      matchList.push(mockData[i]);
+    }
+  }
+  return matchList;
+
+  // for (var i = 0; i < mockData.chair.length; i++) {
+  //   if (
+  //     mockData.chair[i].type.toLowerCase().indexOf(input.toLowerCase()) != -1
+  //   ) {
+  //     matchList.push(mockData.chair[i].type);
+  //   }
+  // }
+};
+
+const displayMatches = matchList => {
+  let j = 0;
+
+  while (j < matchList.length) {
+    results.innerHTML += '<li class="result">' + matchList[j] + '</li>';
+    j++;
+  }
+  toggleResults('show');
 };
